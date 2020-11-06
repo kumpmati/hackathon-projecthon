99b0c3f88ad842302d5fb54b7cd0695cf326c6ae
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-11-06 19:38:33 +0200

    permissions for recording

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.eliteinsmat.hackathonprojecthon
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.justai.aimybox.Aimybox
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
 import java.util.*
 import com.justai.aimybox.core.Config
 import com.justai.aimybox.dialogapi.dialogflow.DialogflowDialogApi
+import com.justai.aimybox.model.Speech
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tts = createAimybox(applicationContext);
+
+        //Permission check for recording audio
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        tts.startRecognition()
     }
     fun createAimybox(context: Context): Aimybox {
         val locale = Locale.getDefault()

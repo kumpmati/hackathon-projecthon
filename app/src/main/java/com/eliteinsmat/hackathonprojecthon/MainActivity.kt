f6569664b4511b081f64c4187beb9a86944e170a
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-11-06 23:58:26 +0200

    new master test

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -31,6 +32,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

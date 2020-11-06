e612854511f4128e3d0ba3e5cab3f60f493c9f11
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-11-06 19:13:36 +0200

    dependencies work after ignoring fails

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
@@ -7,21 +7,23 @@ import com.justai.aimybox.Aimybox
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
 import java.util.*
+import com.justai.aimybox.core.Config
+import com.justai.aimybox.dialogapi.dialogflow.DialogflowDialogApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
-fun createAimybox(context: Context): Aimybox {
-    val locale = Locale.getDefault()
+    fun createAimybox(context: Context): Aimybox {
+        val locale = Locale.getDefault()
 
-    val textToSpeech = GooglePlatformTextToSpeech(context, locale) // Or any other TTS
-    val speechToText = GooglePlatformSpeechToText(context, locale) // Or any other ASR
-    val dialogApi = DialogflowDialogApi(context, R.raw.account, locale.language)
+        val textToSpeech = GooglePlatformTextToSpeech(context, locale) // Or any other TTS
+        val speechToText = GooglePlatformSpeechToText(context, locale) // Or any other ASR
+        val dialogApi = DialogflowDialogApi(context, R.raw.b8584723b0bb, locale.language)
 
-    val config = Config.create(speechToText, textToSpeech, dialogApi)
+        val config = Config.create(speechToText, textToSpeech, dialogApi)
 
-    return Aimybox(config)
+        return Aimybox(config)
+    }
 }

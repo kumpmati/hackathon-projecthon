9489b5aabdac3fc5e2ea0087c0449eb9362bbb40
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-11-06 18:36:23 +0200

    pre merge

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.eliteinsmat.hackathonprojecthon
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.justai.aimybox.Aimybox
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+fun createAimybox(context: Context): Aimybox {
+    val locale = Locale.getDefault()
+
+    val textToSpeech = GooglePlatformTextToSpeech(context, locale) // Or any other TTS
+    val speechToText = GooglePlatformSpeechToText(context, locale) // Or any other ASR
+    val dialogApi = DialogflowDialogApi(context, R.raw.account, locale.language)
+
+    val config = Config.create(speechToText, textToSpeech, dialogApi)
+
+    return Aimybox(config)
 }

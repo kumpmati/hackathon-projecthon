e4bf0ed9fa3c6dbc5a0c264002ac7124ec010bb6
Patrik Larsen <patrik@patriklarsen.com>
2020-11-07 03:09:43 +0200

    maps!

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainActivity.kt
@@ -1,37 +1,48 @@
 package com.eliteinsmat.hackathonprojecthon
 
 import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.justai.aimybox.Aimybox
-import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
-import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
-import java.util.*
 import com.justai.aimybox.core.Config
 import com.justai.aimybox.dialogapi.dialogflow.DialogflowDialogApi
 import com.justai.aimybox.extensions.dialogApiEventsObservable
 import com.justai.aimybox.extensions.textToSpeechEventsObservable
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToTextException
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tts = createAimybox(applicationContext);
+
+        //maps
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
 //UI
 
@@ -61,12 +72,12 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val restauraunts = ArrayList<Restaurant>()
 
-            restauraunts.add(Restaurant("res1"));
-            restauraunts.add(Restaurant("resdsas2"));
-            restauraunts.add(Restaurant("res3213"));
-            restauraunts.add(Restaurant("reasds4"));
-            restauraunts.add(Restaurant("res35"));
-            restauraunts.add(Restaurant("res2333316"));
+            restauraunts.add(Restaurant("res1", LatLng(61.4417671, 22.2842563)));
+            restauraunts.add(Restaurant("resdsas2", LatLng(60.4312671, 21.2842563)));
+            restauraunts.add(Restaurant("res3213",LatLng(60.2117671, 22.3342563)));
+            restauraunts.add(Restaurant("reasds4", LatLng(60.3537671, 22.2841233)));
+            restauraunts.add(Restaurant("res35",LatLng(61.3217671, 22.2312563)));
+            restauraunts.add(Restaurant("res2333316", LatLng(69.4417671, 24.2042563)));
             val adapter = RestaurantAdapter(restauraunts)
 
             ObjectAnimator.ofFloat(relativeView, "translationY", 15f).apply {
@@ -99,6 +110,7 @@ class MainActivity : AppCompatActivity() {
             )
             
             recyclerView.adapter = adapter
+            addMarkers(restauraunts);
         }
 
 //END OF UI
@@ -117,5 +129,18 @@ class MainActivity : AppCompatActivity() {
         val config = Config.create(speechToText, textToSpeech, dialogApi)
 
         return Aimybox(config)
+    }
+
+    fun addMarkers(locations: ArrayList<Restaurant>){
+        locations.forEach {
+            println(it.name)
+         mMap.addMarker(MarkerOptions().position(it.location).title(it.name))
+        }
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val alko = LatLng(60.4417671, 22.2842563)
+        mMap.addMarker(MarkerOptions().position(alko).title("Lähin Alko"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(alko, 15f))
     }
 }
--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/Restaurant.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/Restaurant.kt
@@ -1,6 +1,8 @@
 package com.eliteinsmat.hackathonprojecthon
 
+import com.google.android.gms.maps.model.LatLng
 
-class Restaurant(val name: String) {
+
+class Restaurant(val name: String, val location: LatLng) {
 
 }

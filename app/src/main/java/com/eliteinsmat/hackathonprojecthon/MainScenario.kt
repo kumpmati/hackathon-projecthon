b8289e3afa7ae6451848706cfde3e14008c668b1
Tuomas Rinne <tuomas.rin@hotmail.com>
2020-11-07 15:32:35 +0200

    ravintola tilakone

--- app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainScenario.kt
+++ app/src/main/java/com/eliteinsmat/hackathonprojecthon/MainScenario.kt
@@ -14,12 +14,25 @@ object MainScenario: Scenario() {
                 reactions.sayRandom("What would you like to eat?")
             }
         }
-        state("FoodType") {
-            activators { regex("chinese") }
 
-            action {
-                reactions.say("Here are some chinese restaurants")
-            }
+        state("Chinese") { activators { regex(".*Chinese.*")}
+            action { reactions.say("Here are some chinese restaurants") }
+        }
+
+        state("Burger") { activators { regex(".*Burger.*")}
+            action { reactions.say("Here are some burger restaurants") }
+        }
+
+        state("Indian") { activators { regex(".*Indian.*")}
+            action { reactions.say("Here are some indian restaurants") }
+        }
+
+        state("Japanese") { activators { regex(".*Sushi.*")}
+            action { reactions.say("Here are some sushi restaurants") }
+        }
+
+        state("coffee") { activators { regex(".*Coffee.*")}
+            action { reactions.say("Here are some coffee shops") }
         }
     }
 }
